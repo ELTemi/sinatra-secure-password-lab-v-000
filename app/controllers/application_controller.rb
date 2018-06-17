ba0d46906d53df74e774e7f5c62e08c6e3ca3721
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-17 23:57:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,8 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #@user = User.new(username: params[:username], password: params[:password])
-    if params[:username] == "" && params[:password] == ""
+    if params[:username] == "" !! params[:password] == ""
       redirect to '/failure'
-      
     else
       binding.pry
       @user = User.create(username: params[:username], password: params[:password])
